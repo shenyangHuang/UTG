5a7cfc12e337878254e89e624ce1dd5d87d4d0e0
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2024-04-01 17:51:28 -0400

    run

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=long #unkillable #main #long
-#SBATCH --output=tgn_uci_lr001.txt 
-#SBATCH --error=tgn_uci_lr001_error.txt 
+#SBATCH --output=tgn_uci_lr0001.txt 
+#SBATCH --error=tgn_uci_lr0001_error.txt 
 #SBATCH --cpus-per-task=4                     # Ask for 4 CPUs
 #SBATCH --gres=gpu:1                  # Ask for 1 titan xp gpu:rtx8000:1 
 #SBATCH --mem=32G #64G                             # Ask for 32 GB of RAM
@@ -11,6 +11,22 @@ export HOME="/home/mila/h/huangshe"
 module load python/3.9
 source $HOME/tgbenv/bin/activate
 pwd
+
+
+
+#* for TGN
+
+# python dtdg_tgn.py -d enron -t monthly --lr 0.001 --max_epoch 500 --seed 1 --num_runs 5 --patience 100
+
+python dtdg_tgn.py -d uci -t weekly --lr 0.001 --max_epoch 500 --seed 1 --num_runs 5 --patience 100
+
+# python dtdg_tgn.py -d mooc -t daily --lr 0.001 --max_epoch 500 --seed 1 --num_runs 5 --patience 100
+
+# python dtdg_tgn.py -d social_evo -t daily --lr 0.001 --max_epoch 500 --seed 1 --num_runs 5 --patience 100
+
+# python dtdg_tgn.py -d contacts -t hourly --lr 0.0001 --max_epoch 200 --seed 1 --num_runs 5 --patience 50
+
+
 
 #* for utg 
 
@@ -58,17 +74,6 @@ pwd
 # CUDA_VISIBLE_DEVICES=0 python -u ctdg_main_htgn.py --model=HTGN --dataset=tgbl-coin -t hourly --lr 0.001 --max_epoch 200 --num_runs 5 --patience 50
 
 
-#* for TGN
-
-# python dtdg_tgn.py -d enron -t monthly --lr 0.001 --max_epoch 500 --seed 1 --num_runs 5 --patience 100
-
-python dtdg_tgn.py -d uci -t weekly --lr 0.001 --max_epoch 500 --seed 1 --num_runs 5 --patience 100
-
-# python dtdg_tgn.py -d mooc -t daily --lr 0.001 --max_epoch 500 --seed 1 --num_runs 5 --patience 100
-
-# python dtdg_tgn.py -d social_evo -t daily --lr 0.001 --max_epoch 500 --seed 1 --num_runs 5 --patience 100
-
-# python dtdg_tgn.py -d contacts -t hourly --lr 0.0001 --max_epoch 200 --seed 1 --num_runs 5 --patience 50
 
 
 
