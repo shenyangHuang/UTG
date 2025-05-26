860910750629289aab38744033d1341d518c07d6
Andy Huang <shenyang.huang@mail.mcgill.ca>
2025-05-26 22:13:49 +0100

    changing to UTG

--- plot_og_bar.py
+++ plot_og_bar.py
@@ -7,7 +7,7 @@ data = pd.DataFrame({
     'Dataset': ['Enron', 'LastFM', 'Reddit', 'UCI', 'tgbl-wiki'] * 2,
     'Time': [1.442472665, 27.07388986, 18.08228814, 1.638175269, 4.864469989, 0.5302, 4.8937,
               3.1084, 0.2002, 2.0578],
-    'Method': ['DyGLib'] * 5 + ['OpenDG'] * 5
+    'Method': ['UTG'] * 5 + ['OpenDG'] * 5
 })
 
 # Create grouped bar plot
