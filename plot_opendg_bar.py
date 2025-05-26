bc1e43496d3bbf0bb51aaa2965d888930a043a49
Andy Huang <shenyang.huang@mail.mcgill.ca>
2025-05-26 20:30:30 +0100

    adding bar plot for opendg

---
+++ plot_opendg_bar.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Example data
+data = pd.DataFrame({
+    'Dataset': ['Enron', 'LastFM', 'Reddit', 'UCI', 'tgbl-wiki'] * 2,
+    'Time': [1.442472665, 27.07388986, 18.08228814, 1.638175269, 4.864469989, 0.5302, 4.8937,
+              3.1084, 0.2002, 2.0578],
+    'Method': ['DyGLib'] * 5 + ['OpenDG'] * 5
+})
+
+# Create grouped bar plot
+sns.barplot(data=data, x='Dataset', y='Time', hue='Method')
+plt.ylabel('Score')
+plt.xlabel('Dataset')
+plt.legend()
+plt.savefig('discretizationc_bar.pdf',bbox_inches='tight')
